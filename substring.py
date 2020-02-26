eb95b7d584ce003cc6466909b003358f3b20c87a
dinu1763 <dinu1763@gmail.com>
2020-02-26 06:34:41 -0800

    max length of the substring

---
+++ substring.py
@@ -0,0 +1,11 @@
+def lengthOfLongestSubstring(s):
+    d,m,c,st={},0,0,0
+    for i, e in enumerate(s):
+        if e in d and d[e]>=st: m,c,st=max(m,c),i-d[e],d[e]+1
+        else: c+=1
+        d[e]=i
+    return max(m,c)
+
+print(lengthOfLongestSubstring('abcabcaa'))
+
+print(lengthOfLongestSubstring('pwwkew'))
